af99106aa2d66ec09340255512c1d9afe6ea6617
Fengyan Tina <alwarzontina @gmail.com>
2022-11-08 13:06:04 +0100

    room manager

--- RoomManager.cs
+++ RoomManager.cs
@@ -1,4 +1,28 @@
 class RoomManager
 {
-    
+     private List<Room> rooms ;
+
+
+   
+
+    public List<Room> ShowAvailableRoom()
+    {
+        List<Room> availableRooms =new();
+      return availableRooms ;
+    }
+
+    public void BookRoom()
+    {
+
+    }
+
+     public void AddRoom()
+    {
+
+    }
+
+    public void RemoveRoom()
+    {
+
+    }
 }
