296140944d337b5bfad3b4c3a53c3a70431ceccd
Jessica Trapp <jessica.maria.trapp@gmail.com>
2022-11-10 13:12:59 +0100

    customer menu loop

--- UI/Program.cs
+++ UI/Program.cs
@@ -55,57 +55,56 @@ internal class Program
             switch (choice)
             {
                 case MenuChoiceStaff.ShowRoom:
-                    Console.WriteLine("All rooms!");
-                    foreach (var item in roomManager.ShowAllRooms())
-                    {
-                        Console.WriteLine(item);
-                    }
-                    Console.WriteLine("Available rooms!");
-                    foreach (var item in roomManager.ShowAvailableRoom())
-                    {
-                        Console.WriteLine(item);
-                    }
-                    Console.ReadKey();
-                    break;
+                Console.WriteLine("All rooms!");
+                foreach (var item in roomManager.ShowAllRooms())
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("Available rooms!");
+                foreach (var item in roomManager.ShowAvailableRoom())
+                {
+                    Console.WriteLine(item);
+                }
+                Console.ReadKey();
+                break;
 
                 case MenuChoiceStaff.CheckIn:
-                    Console.WriteLine("Check in/Check out");
-                    break;
+                Console.WriteLine("Check in/Check out");
+                break;
 
                 case MenuChoiceStaff.AddRoom: // and also RemoveRoom()
-                    AddRoomMenyInput();
-
-                    break;
+                AddRoomMenyInput();
+                break;
+                
                 case MenuChoiceStaff.RemoveRoom:
-
-                    Console.WriteLine("room Id: ");
-                    int id = int.Parse(Console.ReadLine());
-                    roomManager.RemoveRoom(id);
-                    break;
+                Console.WriteLine("room Id: ");
+                int id = int.Parse(Console.ReadLine());
+                roomManager.RemoveRoom(id);
+                break;
 
                 case MenuChoiceStaff.Receipt:
-                    Console.WriteLine("Print Receipt");
-                    break;
+                Console.WriteLine("Print Receipt");
+                break;
 
                 case MenuChoiceStaff.Update:
-                    Console.WriteLine("Update room status");
-                    foreach (var item in roomManager.ShowAllRooms())
-                    {
-                        Console.WriteLine(item);
-                    }
-                    Console.WriteLine("Choose room to update: ");
-                    string roomToUpdate = Console.ReadLine();
-                    Console.WriteLine("Choose room status: \n [1] checked in \n [2] check out \n [3] reserved \n [4] not in use");
-                    string newRoomStatus = Console.ReadLine();
-                    roomManager.UpdateRoomStatusID(roomToUpdate, newRoomStatus);
-                    break;
+                Console.WriteLine("Update room status");
+                foreach (var item in roomManager.ShowAllRooms())
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("Choose room to update: ");
+                string roomToUpdate = Console.ReadLine();
+                Console.WriteLine("Choose room status: \n [1] checked in \n [2] check out \n [3] reserved \n [4] not in use");
+                string newRoomStatus = Console.ReadLine();
+                roomManager.UpdateRoomStatusID(roomToUpdate, newRoomStatus);
+                break;
 
                 case MenuChoiceStaff.Quit:
-                    quit = false;
-                    break;
+                quit = true;
+                break;
 
                 default:
-                    break;
+                break;
             }
         }
     }
@@ -152,36 +151,41 @@ internal class Program
 
     private static void GetCustomerInput()
     {
-        Console.WriteLine("Select one of the options:");
-        int CustomerInput = int.Parse(Console.ReadLine());
-        MenuChoiceCustomer CustomerChoice = (MenuChoiceCustomer)CustomerInput;
-
-
-        switch (CustomerChoice)
+        bool quit = true;
+        while (quit)
         {
-            case MenuChoiceCustomer.ViewRoom:
+            Console.WriteLine("Select one of the options:");
+            int CustomerInput = int.Parse(Console.ReadLine());
+            MenuChoiceCustomer CustomerChoice = (MenuChoiceCustomer)CustomerInput;
+
+
+            switch (CustomerChoice)
+            {
+                case MenuChoiceCustomer.ViewRoom:
                 Console.WriteLine("View Available Rooms");
                 break;
 
-            case MenuChoiceCustomer.ViewReviews:
+                case MenuChoiceCustomer.ViewReviews:
                 Console.WriteLine("View Reviews");
                 break;
 
-            case MenuChoiceCustomer.BookRoom:
+                case MenuChoiceCustomer.BookRoom:
                 Console.WriteLine("Book Room");
                 break;
 
-            case MenuChoiceCustomer.WriteReview:
-                Console.WriteLine("Write Review");
+                case MenuChoiceCustomer.WriteReview:
+                Console.WriteLine("Please, Write your review, make sure to add your account:");
+                string review = Console.ReadLine();
                 break;
 
-            case MenuChoiceCustomer.Quit:
-                Console.WriteLine("Quit");
+                case MenuChoiceCustomer.Quit:
+                Console.WriteLine("You have chosen to quit the program");
+                quit=false; 
                 break;
-
-
-            default:
+                
+                default:
                 break;
+            }
         }
     }
 
