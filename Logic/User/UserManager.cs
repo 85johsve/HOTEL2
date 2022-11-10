970a8cc9d744eae364e884162f3c5672d502c947
Jessica Trapp <jessica.maria.trapp@gmail.com>
2022-11-10 15:29:39 +0100

    started with UserManager

---
+++ Logic/User/UserManager.cs
@@ -0,0 +1,40 @@
+using Dapper;
+using MySqlConnector;
+
+class userManager
+{
+    public void CreateEmployee()
+    {
+
+    }
+    public void UpdateEmployee()
+    {
+
+    }
+    // public void List<Employee> ShowAllEmployees()
+    // {
+
+    // }
+    
+    // public void List<Employee> DeleteEmployee()
+    // {
+
+    // }
+    public void CreateCustomer()
+    {
+
+    }
+    public void UpdateCustomer()
+    {
+
+    }
+    // public void List<Customer> ShowAllCustomers()
+    // {
+
+    // }
+    
+    // public void List<Customer> DeleteCustomer()
+    // {
+
+    // }
+}
