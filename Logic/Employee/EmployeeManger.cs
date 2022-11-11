181aac0f8ef544386e2382bf22b71a93159c78a7
Jessica Trapp <jessica.maria.trapp@gmail.com>
2022-11-11 15:04:10 +0100

    more menu choices

--- Logic/Employee/EmployeeManger.cs
+++ Logic/Employee/EmployeeManger.cs
@@ -12,33 +12,6 @@ class EmployeeManager
         
     }
 
-    
-
-<<<<<<< HEAD
-    public int AddEmployee(int jobTitle_id, string employee_fname, string employee_lname, int employee_phone, string employee_email)
-     {
-         int InsertEmployeeID= newEmployeeData.InsertEmployee (jobTitle_id, employee_fname, employee_lname, employee_phone, employee_email);     
-         return InsertEmployeeID; 
-     } 
-    
-    //     using (var connection = new MySqlConnection("Server=localhost;Database=hotelmg;Uid=Tina;Pwd=123456;"))
-    //     {
-            
-    //         Console.WriteLine("Job Title ID: "); //LÄGGA DETTA I PROGRAM
-    //         int addJobTitleId = int.Parse(Console.ReadLine());
-    //         Console.WriteLine("First name: ");
-    //         string addEmployeeFName = Console.ReadLine();
-    //         Console.WriteLine("Last name: ");
-    //         string addEmployeeLName = Console.ReadLine();
-    //         Console.WriteLine("Phone: ");
-    //         int addEmployeePhone = int.Parse(Console.ReadLine());
-    //         Console.WriteLine("Email: ");
-    //         string addEmployeeEmail = Console.ReadLine();
-    //         var employee = connection.Query<Employee>($@"INSERT INTO employees(jobTitle_id, employee_fname, 
-    //         employee_lname, employee_phone, employee_email)VALUES ('{addJobTitleId}', '{addEmployeeFName}'
-    //         '{addEmployeeLName}','{addEmployeePhone}','{addEmployeeEmail}',);").ToList();
-    //         Console.WriteLine("Employee has been added!");
-    //     }
 
     public int AddEmployee(int jobId, string fname, string lname, int phone, string email)
     {
@@ -46,7 +19,6 @@ class EmployeeManager
         return InsertEmployeeID;  
     
         
->>>>>>> 10fc4ad4f095a2462ea6a605913581fc231f984a
     
      }
         public List<Employee>ShowAllEmployees()
