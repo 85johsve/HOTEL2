2c6023b0605c8757a61e1bb6989acde1b2726de7
Fengyan Tina <alwarzontina @gmail.com>
2022-11-15 15:30:19 +0100

    review class properties is done

--- Logic/Review/Review.cs
+++ Logic/Review/Review.cs
@@ -2,12 +2,13 @@ class Review
 {
     public int review_id {get; set;}
     public int reservation_id {get; set;} //Do we need this?
-    public int customer_name {get; set;}
-    public string review_content {get; set;}
+    public int customer_id {get; set;}
+    public string? review_content {get; set;}
+     public string? customer_fname {get; set;}
 
     
     public override string ToString()
     {
-        return $"Review: " + review_id + " " + reservation_id + " " + customer_name + " " + review_content;
+        return $"Review: " + review_id + "\nReservation:" + reservation_id + "\nCustomer Account " + customer_id + "\nCustomer Name " + customer_fname + "\n" + review_content;
     }
 }
