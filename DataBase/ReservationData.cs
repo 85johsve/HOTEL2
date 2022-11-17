f2a7da4949fa76f38fe8dc90c37b2a1b1db66c65
Fengyan Tina <alwarzontina @gmail.com>
2022-11-17 09:43:17 +0100

    change singquatry reservation data

--- DataBase/ReservationData.cs
+++ DataBase/ReservationData.cs
@@ -54,9 +54,9 @@ LEFT JOIN reservations ON rooms.room_id = reservations.room_id").ToList();
         
     }
 
-    public List<Reservation> GetSingleReservation(int reservation_id)
+    public Reservation GetSingleReservation(int reservation_id)
     {
-        var getSingleResrvation = connection.Query<Reservation> ($"SELECT * FROM `reservations` WHERE reservations.reservation_id = {reservation_id};").ToList();
+        var getSingleResrvation = connection.QuerySingle<Reservation> ($"SELECT * FROM `reservations` WHERE reservations.reservation_id = {reservation_id};");
         return getSingleResrvation;
     }
 
