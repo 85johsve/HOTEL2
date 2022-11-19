69d8b92de29552d5e43ad675db2a381611017a65
Fengyan Tina <alwarzontina @gmail.com>
2022-11-19 12:06:08 +0100

    receipt print change

--- Logic/Payment/Receipt.cs
+++ Logic/Payment/Receipt.cs
@@ -3,11 +3,10 @@ public class Receipt
     public DateTime print_date{get;set;}
     public static int receipt_nr{get;set;}
     Reservation reservation =new();
-    public int id;
-    ReservationManager reservationManager =new();
+   
 
     public override string ToString()
     {
-        return $@"Date: " + print_date + "\nReceipt Nr: " + receipt_nr + reservationManager.SearchReservationById(id);
+        return $@"Date: " + print_date + "\nReceipt Nr: " + receipt_nr ;
     }
 }
