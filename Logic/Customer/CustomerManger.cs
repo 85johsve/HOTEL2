3e8bc6d6dba14adbb3db275c23276585e7cf7f9b
Fengyan Tina <alwarzontina @gmail.com>
2022-11-22 12:18:01 +0100

    customer is done cleaning

--- Logic/Customer/CustomerManger.cs
+++ Logic/Customer/CustomerManger.cs
@@ -4,28 +4,22 @@ using MySqlConnector;
 public class CustomerManager
 {
 
-    // private List<Customer> customers;
     CustomerData newCustomerData = new();
 
     public int AddCustomer(string fname, string lname, int phone, string email, string city, string country, string address)
     {
         int insertCustomerID = newCustomerData.InsertCustomer(fname, lname, phone, email, city, country, address);
-
         return insertCustomerID;
     }
 
     public Customer SearchCustomerById(int sCustomerId)
     {
-        
-         return newCustomerData.GetCustomerById(sCustomerId);
-       
+        return newCustomerData.GetCustomerById(sCustomerId);
     }
 
     public List<Customer> ShowAllCustomers()
     {
-       
         return newCustomerData.GetCustomerList();
-        
     }
 
     public void RemoveCustomerById(int rCustomerId)
@@ -33,14 +27,13 @@ public class CustomerManager
         newCustomerData.DeleteCustomerById(rCustomerId);
     }
 
-    public bool CustomerLogInNameId(int id,string fname )
+    public bool CustomerLogInNameId(int id, string fname)
     {
-       //return newCustomerData.IsValidUser(fname,id);
-       return newCustomerData.GetCustomerLogInNameId(id,fname);
+        return newCustomerData.GetCustomerLogInNameId(id, fname);
     }
 
     public string CustomerLogInPass(int id)
     {
-         return newCustomerData.GetCustomerLogInPass(id);
+        return newCustomerData.GetCustomerLogInPass(id);
     }
 }
